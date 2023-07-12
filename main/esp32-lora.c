ca507d7b81a0e846dceff172b2c17cb7c8d9962b
Alejandro E. Rendon <alejandro@rendon.co>
2023-07-11 23:15:43 -0500

    Refactor to re-use common methods

--- main/esp32-lora.c
+++ main/esp32-lora.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 #include "ssd1306.h"
 
 SSD1306_t screen;
 
-void app_main(void)
+void screen_init()
 {
   i2c_master_init(&screen, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
   ssd1306_init(&screen, 128, 64);
-  // Clear the screen
-  ssd1306_clear_screen(&screen, false);
   // Set contrast
   ssd1306_contrast(&screen, 0xff);
+}
+
+// Clear the screen
+void screen_clear()
+{
+  ssd1306_clear_screen(&screen, false);
+}
+
+void screen_print(char *str, int page)
+{
   // Clear first line
-  ssd1306_clear_line(&screen, 0, false);
+  ssd1306_clear_line(&screen, page, false);
   // Display text
-  ssd1306_display_text(&screen, 0, "Hello World!", 12, false);
+  ssd1306_display_text(&screen, page, str, strlen(str), false);
+}
+
+void app_main(void)
+{
+  screen_init();
+  screen_clear();
+  screen_print("Hello World!", 0);
 }
